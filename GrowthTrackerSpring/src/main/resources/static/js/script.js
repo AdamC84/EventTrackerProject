357c08dba56e6cca3c282e07aee7b440a4f5e3f7
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-06-09 21:48:20 -0600

    commit

--- GrowthTrackerSpring/src/main/resources/static/js/script.js
+++ GrowthTrackerSpring/src/main/resources/static/js/script.js
@@ -1,26 +1,18 @@
 window.addEventListener('load', function(e) {
 	console.log('document loaded');
 	console.log('loaded');
+	console.log('loaded again');
 	init();
 });
 
 function init() {
 	console.log("working");
-	var tds = document.getElementsByTagName('td');
 	loadEvents();
 	document.addEventForm.add.addEventListener('click', function(event) {
 		console.log('working');
 		event.preventDefault();
 		addEvent();
 	});
-	for (let i = 0; i < tds.length; i++) {
-		console.log(tds[i]);
-		tds[i].addEventListener('click', function(event) {
-			console.log('working');
-			event.preventDefault();
-			event.target
-		});
-	}
 }
 function loadEvents() {
 	var xhr = new XMLHttpRequest();
@@ -74,22 +66,53 @@ function addEvent() {
 
 	document.addEventForm.reset();
 }
+function getEventById(id){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/event/' + id);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status < 400) {
+			var data = JSON.parse(xhr.responseText);
+			 console.log(data);
+			console.log(xhr.responseText);
+			return data;
+
+		}
+
+		if (xhr.readyState === 4 && xhr.status >= 400) {
+			console.error(xhr.status + ': ' + xhr.responseText);
+			div.textContent = xhr.responseText;
+		}
+	};
+	xhr.send(null);
+}
+
 function displayEvents(data) {
 	var eventDiv = document.getElementById('loadedEvents');
 	var tbody = document.createElement('tbody');
-	var tr = document.createElement('tr');
 	var table = document.createElement('table');
 	eventDiv.appendChild(table);
 	table.appendChild(tbody);
-	tbody.appendChild(tr);
 	var array1 = [ 'category', 'fName', 'lName', 'description', 'date' ];
 	for (let i = 0; i < data.length; i++) {
+		var tr = document.createElement('tr');
+		tr.value = data[i].id;
+		tbody.appendChild(tr);
+		tr.textContent = data[i].category;
 		for (let j = 0; j < array1.length; j++) {
-			var td = document.createElement('td');
-			tr.appendChild(td);
-			// console.log(data[i]);
-			td.textContent = data[i][array1[j]];
+				// console.log(data[i]);
+				var td = document.createElement('td');
+				tr.appendChild(td);
+				console.log(data[i].id)
+//				td.textContent = data[i][array1[j]];
 
 		}
 	}
+	var trs = document.getElementsByTagName('tr');
+	for (let k = 0; k < trs.length; k++)
+	trs[k].addEventListener('click', function(event) {
+		console.log(event.target.value);
+		let obj = getEventById(event.target.value);
+		console.log(obj);
+//					displayEvent();
+	});
 }
