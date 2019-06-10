9aba8b45c6b75e5965190388d38444ac2e78e503
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-06-10 08:24:15 -0600

    commit

--- GrowthTrackerSpring/src/main/resources/static/js/script.js
+++ GrowthTrackerSpring/src/main/resources/static/js/script.js
@@ -66,13 +66,13 @@ function addEvent() {
 
 	document.addEventForm.reset();
 }
-function getEventById(id){
+function getEventById(id) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'api/event/' + id);
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function getId() {
 		if (xhr.readyState === 4 && xhr.status < 400) {
-			var data = JSON.parse(xhr.responseText);
-			 console.log(data);
+			data = JSON.parse(xhr.responseText);
+			console.log(data);
 			console.log(xhr.responseText);
 			return data;
 
@@ -84,35 +84,35 @@ function getEventById(id){
 		}
 	};
 	xhr.send(null);
+	var obj = getId();
+	return obj;
 }
-
+var obj1 = getEventById(1);
+console.log(obj1);
 function displayEvents(data) {
+
 	var eventDiv = document.getElementById('loadedEvents');
 	var tbody = document.createElement('tbody');
 	var table = document.createElement('table');
 	eventDiv.appendChild(table);
 	table.appendChild(tbody);
-	var array1 = [ 'category', 'fName', 'lName', 'description', 'date' ];
 	for (let i = 0; i < data.length; i++) {
 		var tr = document.createElement('tr');
 		tr.value = data[i].id;
 		tbody.appendChild(tr);
 		tr.textContent = data[i].category;
-		for (let j = 0; j < array1.length; j++) {
-				// console.log(data[i]);
+		var trs = document.getElementsByTagName('tr');
+//		var obj = getEventById(1);
+//		console.log(obj);
+		trs[i].addEventListener('click', function(event) {
+			console.log(event.target.value);
+			console.log(getEventById(event.target.value));
+			for (let k = 0; k < data.length; k++) {
 				var td = document.createElement('td');
 				tr.appendChild(td);
-				console.log(data[i].id)
-//				td.textContent = data[i][array1[j]];
+			}
+			// td.textContent = data[i][array1[j]];
 
-		}
+		});
 	}
-	var trs = document.getElementsByTagName('tr');
-	for (let k = 0; k < trs.length; k++)
-	trs[k].addEventListener('click', function(event) {
-		console.log(event.target.value);
-		let obj = getEventById(event.target.value);
-		console.log(obj);
-//					displayEvent();
-	});
 }
